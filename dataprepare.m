e8db9c3a3585ed50d3936658faa74bc297e82662
Narges S. Bathaeian <narges.s.bathaeian@gmail.com>
2023-08-15 10:57:55 +0330

    Update dataprepare.m

--- dataprepare.m
+++ dataprepare.m
@@ -1,9 +1,12 @@
-subj2=char(strcat('00' ,string(table2cell(COBREphenotypicdata(1,1))))) ;
+lData= size(COBREphenotypicdata,1);
+for i=1:lData
+subj2=char(strcat('00' ,string(table2cell(COBREphenotypicdata(i,1))))) ;
 rest_fn = [subj2 filesep 'session_1' filesep 'rest_1' filesep 'rest.nii.gz'];
 file_rest = gunzip(rest_fn);
-if char(string(table2cell(COBREphenotypicdata(1,5))))=='Control' 
+if char(string(table2cell(COBREphenotypicdata(i,5))))=='Control' 
 dest=['Control' filesep subj2 '.nii']
 else 
  dest=['Patient' filesp subj2 '.nii']
 end;
 movefile(file_rest{1},dest)
+end;
