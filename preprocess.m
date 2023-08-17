b4e023eb2d32eddccf51e0b8f781d7364d7722e2
Narges S. Bathaeian <narges.s.bathaeian@gmail.com>
2023-08-17 09:53:16 +0330

    Update preprocess.m

--- preprocess.m
+++ preprocess.m
@@ -1,2 +1,11 @@
 addpath D:\student\research\spm\spm12
 spm fmri
+
+files = dir('wr*.nii');
+numFiles = numel(files);
+for i = 1:numFiles
+  if ismember(r,UNI)
+        continue
+  end
+    UNI = [UNI; r];
+end
