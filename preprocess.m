e4922e1c64b31bc1355024302158de33dbfe00f4
Narges S. Bathaeian <narges.s.bathaeian@gmail.com>
2023-08-17 10:12:57 +0330

    Update preprocess.m

--- preprocess.m
+++ preprocess.m
@@ -3,10 +3,12 @@ spm fmri
 p='E:\COBRE\Control\';
 files = dir([p 'wr*.nii']);
 numFiles = numel(files);
+numVol=150;
 for i = 1:numFiles
-  r=[p files(i).name];
-  if ismember(r,matlabbatch{1,1}.spm.spatial.smooth.data)
-        continue
-  end
-    matlabbatch{1,1}.spm.spatial.smooth.data = [matlabbatch{1,1}.spm.spatial.smooth.data; r];
+  for j=1:numVol
+    r=[p files(i).name ',' int2str(j)];
+    if !ismember(r,matlabbatch{1,1}.spm.spatial.smooth.data)
+        matlabbatch{1,1}.spm.spatial.smooth.data = [matlabbatch{1,1}.spm.spatial.smooth.data; r];
+    end
+  end  
 end
