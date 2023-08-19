920b876ca7c4519487deed6f1840901e176e46f4
Narges S. Bathaeian <narges.s.bathaeian@gmail.com>
2023-08-19 12:24:14 +0330

    Update deepData.m

--- deepData.m
+++ deepData.m
@@ -18,3 +18,21 @@ for i = 1:numFiles
   newF= strrep(files(i).name,'.nii','.mat'); 
   save([p2 newF],"newImg3");
 end;
+
+
+files = dir([p1 'swr*.mat']);
+figure(1) 
+
+filename = 'FMRI_GIF.gif'; 
+A= files(1).newImg3;
+NumV=size(A,3);
+for k = 1:NumV imshow(A(:,:,k),[]) 
+drawnow 
+frame = getframe(1); 
+im = frame2im(frame); 
+[imind,cm] = rgb2ind(im,256); 
+if k == 1; 
+imwrite(imind,cm,filename,'gif', 'Loopcount',inf); else imwrite(imind,cm,filename,'gif','WriteMode','append'); 
+end 
+pause(.1) 
+end
