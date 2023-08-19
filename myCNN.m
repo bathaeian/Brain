971cfbac7f27919724212d5ef4dfa301de90ff3a
Narges S. Bathaeian <narges.s.bathaeian@gmail.com>
2023-08-19 16:13:28 +0330

    Create myCNN.m

---
+++ myCNN.m
@@ -0,0 +1,33 @@
+fmriDatasetPath = fullfile('AllData','10');
+fmris = imageDatastore(digitDatasetPath,'IncludeSubfolders',true,'LabelSource','foldernames');
+labelCount = countEachLabel(fmris);
+numTrainFiles = 750;
+[imdsTrain,imdsValidation] = splitEachLabel(imds,numTrainFiles,'randomize');
+layers = [
+imageInputLayer([28 28 1])
+convolution2dLayer(3,8,'Padding','same')
+batchNormalizationLayer
+reluLayer
+maxPooling2dLayer(2,'Stride',2)
+convolution2dLayer(3,16,'Padding','same')
+batchNormalizationLayer
+reluLayer
+maxPooling2dLayer(2,'Stride',2)
+convolution2dLayer(3,32,'Padding','same')
+batchNormalizationLayer
+reluLayer
+fullyConnectedLayer(10)
+softmaxLayer
+classificationLayer]
+options = trainingOptions('sgdm', ...
+'InitialLearnRate',0.01, ...
+'MaxEpochs',4, ...
+'Shuffle','every-epoch', ...
+'ValidationData',imdsValidation, ...
+'ValidationFrequency',30, ...
+'Verbose',false, ...
+'Plots','training-progress');
+net = trainNetwork(imdsTrain,layers,options);
+YPred = classify(net,imdsValidation);
+YValidation = imdsValidation.Labels;
+accuracy = sum(YPred == YValidation)/numel(YValidation)
