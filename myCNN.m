301d12963e5c0340b97275e4297c778ecd209c10
Narges S. Bathaeian <narges.s.bathaeian@gmail.com>
2023-08-20 18:21:02 +0330

    Update myCNN.m

--- myCNN.m
+++ myCNN.m
@@ -1,12 +1,8 @@
 fmriDatasetPath = ['AllData' filesep '10'];
-fmris = imageDatastore(fmriDatasetPath,'IncludeSubfolders',true,'LabelSource','foldernames',"FileExtensions",".mat","ReadFcn",@(x) matRead(x));
+fmris = image3dDatastore(fmriDatasetPath,'IncludeSubfolders',true,'LabelSource','foldernames',"FileExtensions",".mat","ReadFcn",@(x) matRead(x));
 labelCount = countEachLabel(fmris);
 numTrainFiles = int32(min(table2array(labelCount(:,2)))*4/5);
 [fmriTrain,fmriValidation] = splitEachLabel(fmris,numTrainFiles,'randomize');
-outputSize = [52 52 38];
-%%rescale_fmriTrain = transform(fmriTrain, @(x) rescale(x,0,1));
-aug_fmriTrain = augment3d(fmriTrain, outputSize, 'Interpolation', 'linear');%, "ColorPreprocessing","rgb2gray");
-aug_fmriValidation = augment3d(fmriValidation, outputSize, 'Interpolation', 'linear')%;, "ColorPreprocessing","rgb2gray");
 
 layers = [
 image3dInputLayer([53 63 38])
@@ -25,14 +21,14 @@ options = trainingOptions('sgdm', ...
 'InitialLearnRate',0.01, ...
 'MaxEpochs',4, ...
 'Shuffle','every-epoch', ...
-'ValidationData',aug_fmriValidation, ...
+'ValidationData',fmriValidation, ...
 'ValidationFrequency',10, ...
 'Verbose',false, ...
 'Plots','training-progress');
 analyzeNetwork(layers);
-net = trainNetwork(aug_fmriTrain,layers,options);
-YPred = classify(net,aug_fmriValidation);
-YValidation = aug_fmriValidation.Labels;
+net = trainNetwork(fmriTrain,layers,options);
+YPred = classify(net,fmriValidation);
+YValidation = fmriValidation.Labels;
 accuracy = sum(YPred == YValidation)/numel(YValidation)
 function data = matRead(filename)
     inp = load(filename);
